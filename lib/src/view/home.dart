820dbf3319454d1411770f8b8428552d869b6da6
畑山翔斗 <>
2022-09-12 18:19:08 +0900

    【ホーム画面】 ・リストがスクロールするように記述

--- lib/src/view/home.dart
+++ lib/src/view/home.dart
@@ -23,37 +23,43 @@ class _HomeWidgetState extends State<HomeWidget> {
         title: const Text("ホーム"),
       ),
       body: Column(children: <Widget>[
-        CheckboxListTile(
-            activeColor: Colors.blue,
-            title: const Text('期限切れタスク'),
-            subtitle: const Text('期限切れです'),
-            tileColor: Colors.pink[100],
-            controlAffinity: ListTileControlAffinity.leading,
-            value: isChecked,
-            onChanged: _handleCheckbox),
-        CheckboxListTile(
-            activeColor: Colors.blue,
-            title: const Text('本日のタスク'),
-            subtitle: const Text('チェックボックスのサブタイトル'),
-            controlAffinity: ListTileControlAffinity.leading,
-            value: isChecked,
-            onChanged: _handleCheckbox),
-        ExpansionTile(
-          title: const Text('完了済みのタスク'),
-          leading: const Icon(Icons.info),
-          children: [
-            Column(
-              children: <Widget>[
-                CheckboxListTile(
-                    activeColor: Colors.blue,
-                    title: const Text('完了したタスク'),
-                    subtitle: const Text('完了'),
-                    controlAffinity: ListTileControlAffinity.leading,
-                    value: isChecked,
-                    onChanged: _handleCheckbox),
-              ],
-            ),
-          ],
+        SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              CheckboxListTile(
+                  activeColor: Colors.blue,
+                  title: const Text('期限切れタスク'),
+                  subtitle: const Text('期限切れです'),
+                  tileColor: Colors.pink[100],
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: isChecked,
+                  onChanged: _handleCheckbox),
+              CheckboxListTile(
+                  activeColor: Colors.blue,
+                  title: const Text('本日のタスク'),
+                  subtitle: const Text('チェックボックスのサブタイトル'),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: isChecked,
+                  onChanged: _handleCheckbox),
+              ExpansionTile(
+                title: const Text('完了済みのタスク'),
+                leading: const Icon(Icons.check),
+                children: [
+                  Column(
+                    children: <Widget>[
+                      CheckboxListTile(
+                          activeColor: Colors.blue,
+                          title: const Text('完了したタスク'),
+                          subtitle: const Text('完了'),
+                          controlAffinity: ListTileControlAffinity.leading,
+                          value: isChecked,
+                          onChanged: _handleCheckbox),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ]),
     );
